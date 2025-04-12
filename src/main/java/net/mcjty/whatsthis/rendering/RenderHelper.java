6262246d5d9e9c57ffef2e3564d5957a69162c7a
Daniel Fildán <fildand@centrum.cz>
2025-04-13 00:01:08 +0200

    Cleanup RenderHelper

--- src/main/java/net/mcjty/whatsthis/rendering/RenderHelper.java
+++ src/main/java/net/mcjty/whatsthis/rendering/RenderHelper.java
@@ -1,14 +1,12 @@
 package net.mcjty.whatsthis.rendering;
 
 import net.mcjty.whatsthis.WhatsThis;
-import net.minecraft.block.Block;
 import net.minecraft.client.Minecraft;
 import net.minecraft.client.render.Tessellator;
 import net.minecraft.client.render.entity.EntityRenderDispatcher;
 import net.minecraft.client.render.item.ItemRenderer;
 import net.minecraft.client.render.platform.Lighting;
 import net.minecraft.entity.Entity;
-import net.minecraft.item.Item;
 import net.minecraft.item.ItemStack;
 import org.lwjgl.opengl.GL11;
 import org.lwjgl.opengl.GL12;
@@ -16,7 +14,7 @@ import org.lwjgl.opengl.GL13;
 
 import java.awt.*;
 
-@SuppressWarnings("DuplicatedCode")
+@SuppressWarnings({"DuplicatedCode", "JavaExistingMethodCanBeUsed", "UnusedReturnValue"})
 public class RenderHelper {
     public static float rot = 0.0f;
 
@@ -92,31 +90,6 @@ public class RenderHelper {
         GL13.glActiveTexture(GL13.GL_TEXTURE1); // lightmapTexUnit
         GL11.glDisable(GL11.GL_TEXTURE_2D);
         GL13.glActiveTexture(GL13.GL_TEXTURE0); // defaultTexUnit
-    }
-
-    public static boolean renderObject(Minecraft mc, int x, int y, Object itm, boolean highlight) {
-        if (itm instanceof Entity) {
-            renderEntity((Entity) itm, x, y, 10);
-            return true;
-        }
-        ItemRenderer itemRender = new ItemRenderer();
-        return renderObject(mc, itemRender, x, y, itm, highlight, 200);
-    }
-
-    public static boolean renderObject(Minecraft mc, ItemRenderer itemRender, int x, int y, Object itm, boolean highlight, float lvl) {
-        if (itm == null) {
-            return renderItemStack(mc, itemRender, null, x, y, "", highlight);
-        }
-        if (itm instanceof Item) {
-            return renderItemStack(mc, itemRender, new ItemStack((Item) itm, 1), x, y, "", highlight);
-        }
-        if (itm instanceof Block) {
-            return renderItemStack(mc, itemRender, new ItemStack((Block) itm, 1), x, y, "", highlight);
-        }
-        if (itm instanceof ItemStack) {
-            return renderItemStackWithCount(mc, itemRender, (ItemStack) itm, x, y, highlight);
-        }
-        return renderItemStack(mc, itemRender, new ItemStack(0, 0, 0), x, y, "", highlight);
     }
 
     public static boolean renderItemStackWithCount(Minecraft mc, ItemRenderer itemRender, ItemStack stack, int xo, int yo, boolean highlight) {
@@ -369,184 +342,6 @@ public class RenderHelper {
         t.vertex((x), (y), zLevel, ((u) * f), (v) * f1);
         t.draw();
     }
-
-    public static void renderBillboardQuadBright(double scale) {
-//        int brightness = 240;
-//        int b1 = brightness >> 16 & 65535;
-//        int b2 = brightness & 65535;
-//        GlStateManager.pushMatrix();
-//        RenderHelper.rotateToPlayer();
-//        Tessellator tessellator = Tessellator.getInstance();
-//        BufferBuilder buffer = tessellator.getBuffer();
-//        buffer.begin(7, DefaultVertexFormats.POSITION_TEX_LMAP_COLOR);
-//        buffer.pos(-scale, -scale, 0.0D).tex(0.0D, 0.0D).lightmap(b1, b2).color(255, 255, 255, 128).endVertex();
-//        buffer.pos(-scale, scale, 0.0D).tex(0.0D, 1.0D).lightmap(b1, b2).color(255, 255, 255, 128).endVertex();
-//        buffer.pos(scale, scale, 0.0D).tex(1.0D, 1.0D).lightmap(b1, b2).color(255, 255, 255, 128).endVertex();
-//        buffer.pos(scale, -scale, 0.0D).tex(1.0D, 0.0D).lightmap(b1, b2).color(255, 255, 255, 128).endVertex();
-//        tessellator.draw();
-//        GlStateManager.popMatrix();
-    }
-
-    public static void renderBillboardQuad(double scale) {
-//        GlStateManager.pushMatrix();
-//
-//        rotateToPlayer();
-//
-//        Tessellator tessellator = Tessellator.getInstance();
-//        BufferBuilder buffer = tessellator.getBuffer();
-//        buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-//        buffer.pos(-scale, -scale, 0).tex(0, 0).endVertex();
-//        buffer.pos(-scale, +scale, 0).tex(0, 1).endVertex();
-//
-//        buffer.pos(+scale, +scale, 0).tex(1, 1).endVertex();
-//
-//        buffer.pos(+scale, -scale, 0).tex(1, 0).endVertex();
-//
-//        tessellator.draw();
-//        GlStateManager.popMatrix();
-    }
-
-    public static void renderBillboardQuadWithRotation(float rot, double scale) {
-//        GlStateManager.pushMatrix();
-//
-//        rotateToPlayer();
-//
-//        GlStateManager.rotate(rot, 0, 0, 1);
-//
-//        Tessellator tessellator = Tessellator.getInstance();
-//        BufferBuilder buffer = tessellator.getBuffer();
-//        buffer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_TEX);
-//        buffer.pos(-scale, -scale, 0).tex(0, 0).endVertex();
-//        buffer.pos(-scale, +scale, 0).tex(0, 1).endVertex();
-//
-//        buffer.pos(+scale, +scale, 0).tex(1, 1).endVertex();
-//
-//        buffer.pos(+scale, -scale, 0).tex(1, 0).endVertex();
-//
-//        tessellator.draw();
-//        GlStateManager.popMatrix();
-    }
-
-    public static void rotateToPlayer() {
-//        GlStateManager.rotate(-Minecraft.getMinecraft().getRenderManager().playerViewY, 0.0F, 1.0F, 0.0F);
-//        GlStateManager.rotate(Minecraft.getMinecraft().getRenderManager().playerViewX, 1.0F, 0.0F, 0.0F);
-    }
-
-    /**
-     * Draw a beam with some thickness.
-     */
-    public static void drawBeam(Vector S, Vector E, Vector P, float width) {
-//        Vector PS = Sub(S, P);
-//        Vector SE = Sub(E, S);
-//
-//        Vector normal = Cross(PS, SE);
-//        normal = normal.normalize();
-//
-//        Vector half = Mul(normal, width);
-//        Vector p1 = Add(S, half);
-//        Vector p2 = Sub(S, half);
-//        Vector p3 = Add(E, half);
-//        Vector p4 = Sub(E, half);
-//
-//        drawQuad(Tessellator.INSTANCE, p1, p3, p4, p2);
-    }
-
-    private static void drawQuad(Tessellator tessellator, Vector p1, Vector p2, Vector p3, Vector p4) {
-//        int brightness = 240;
-//        int b1 = brightness >> 16 & 65535;
-//        int b2 = brightness & 65535;
-//
-//        BufferBuilder buffer = tessellator.getBuffer();
-//        buffer.pos(p1.getX(), p1.getY(), p1.getZ()).tex(0.0D, 0.0D).lightmap(b1, b2).color(255, 255, 255, 128).endVertex();
-//        buffer.pos(p2.getX(), p2.getY(), p2.getZ()).tex(1.0D, 0.0D).lightmap(b1, b2).color(255, 255, 255, 128).endVertex();
-//        buffer.pos(p3.getX(), p3.getY(), p3.getZ()).tex(1.0D, 1.0D).lightmap(b1, b2).color(255, 255, 255, 128).endVertex();
-//        buffer.pos(p4.getX(), p4.getY(), p4.getZ()).tex(0.0D, 1.0D).lightmap(b1, b2).color(255, 255, 255, 128).endVertex();
-    }
-
-    /**
-     * Renders the stack size and/or damage bar for the given ItemStack.
-     */
-//    public static void renderItemOverlayIntoGUI(FontRenderer fr, ItemStack stack, int xPosition, int yPosition, @Nullable String text,
-//                                                int scaled) {
-//        if (!stack.isEmpty()) {
-//            if (stack.getCount() != 1 || text != null) {
-//                String s = text == null ? String.valueOf(stack.getCount()) : text;
-//                if (text == null && stack.getCount() < 1) {
-//                    s = TextFormatting.RED + String.valueOf(stack.getCount());
-//                }
-//
-//                GlStateManager.disableLighting();
-//                GlStateManager.disableDepth();
-//                GlStateManager.disableBlend();
-//                if (scaled >= 2) {
-//                    GlStateManager.pushMatrix();
-//                    GlStateManager.scale(.5f, .5f, .5f);
-//                    fr.drawStringWithShadow(s, ((xPosition + 19 - 2) * 2 - 1 - fr.getStringWidth(s)), yPosition * 2 + 24, 16777215);
-//                    GlStateManager.popMatrix();
-//                } else if (scaled == 1) {
-//                    GlStateManager.pushMatrix();
-//                    GlStateManager.scale(.75f, .75f, .75f);
-//                    fr.drawStringWithShadow(s, ((xPosition - 2) * 1.34f + 24 - fr.getStringWidth(s)), yPosition * 1.34f + 14, 16777215);
-//                    GlStateManager.popMatrix();
-//                } else {
-//                    fr.drawStringWithShadow(s, (xPosition + 19 - 2 - fr.getStringWidth(s)), (yPosition + 6 + 3), 16777215);
-//                }
-//                GlStateManager.enableLighting();
-//                GlStateManager.enableDepth();
-//                // Fixes opaque cooldown overlay a bit lower
-//                // TODO: check if enabled blending still screws things up down the line.
-//                GlStateManager.enableBlend();
-//            }
-//
-//            if (stack.getItem().showDurabilityBar(stack)) {
-//                double health = stack.getItem().getDurabilityForDisplay(stack);
-//                int j = (int) Math.round(13.0D - health * 13.0D);
-//                int i = (int) Math.round(255.0D - health * 255.0D);
-//                GlStateManager.disableLighting();
-//                GlStateManager.disableDepth();
-//                GlStateManager.disableTexture2D();
-//                GlStateManager.disableAlpha();
-//                GlStateManager.disableBlend();
-//                Tessellator tessellator = Tessellator.getInstance();
-//                BufferBuilder vertexbuffer = tessellator.getBuffer();
-//                draw(vertexbuffer, xPosition + 2, yPosition + 13, 13, 2, 0, 0, 0, 255);
-//                draw(vertexbuffer, xPosition + 2, yPosition + 13, 12, 1, (255 - i) / 4, 64, 0, 255);
-//                draw(vertexbuffer, xPosition + 2, yPosition + 13, j, 1, 255 - i, i, 0, 255);
-//                GlStateManager.enableBlend();
-//                GlStateManager.enableAlpha();
-//                GlStateManager.enableTexture2D();
-//                GlStateManager.enableLighting();
-//                GlStateManager.enableDepth();
-//            }
-//
-//            EntityPlayerSP entityplayersp = Minecraft.getMinecraft().player;
-//            float f = entityplayersp == null ? 0.0F : entityplayersp.getCooldownTracker().getCooldown(stack.getItem(), Minecraft.getMinecraft().getRenderPartialTicks());
-//
-//            if (f > 0.0F) {
-//                GlStateManager.disableLighting();
-//                GlStateManager.disableDepth();
-//                GlStateManager.disableTexture2D();
-//                Tessellator tessellator1 = Tessellator.getInstance();
-//                BufferBuilder vertexbuffer1 = tessellator1.getBuffer();
-//                draw(vertexbuffer1, xPosition, yPosition + (int)Math.floor(16.0F * (1.0F - f)), 16, (int)Math.ceil(16.0F * f), 255, 255, 255, 127);
-//                GlStateManager.enableTexture2D();
-//                GlStateManager.enableLighting();
-//                GlStateManager.enableDepth();
-//            }
-//        }
-//    }
-
-    /**
-     * Draw with the WorldRenderer
-     */
-//    private static void draw(BufferBuilder renderer, int x, int y, int width, int height, int red, int green, int blue, int alpha) {
-//        renderer.begin(GL11.GL_QUADS, DefaultVertexFormats.POSITION_COLOR);
-//        renderer.pos((x + 0), (y + 0), 0.0D).color(red, green, blue, alpha).endVertex();
-//        renderer.pos((x + 0), (y + height), 0.0D).color(red, green, blue, alpha).endVertex();
-//        renderer.pos((x + width), (y + height), 0.0D).color(red, green, blue, alpha).endVertex();
-//        renderer.pos((x + width), (y + 0), 0.0D).color(red, green, blue, alpha).endVertex();
-//        Tessellator.getInstance().draw();
-//    }
     
     public static int renderText(Minecraft minecraft, int x, int y, String text) {
         int width = ProbeTextRenderer.INSTANCE.getWidth(text);
@@ -579,58 +374,4 @@ public class RenderHelper {
 
         ProbeTextRenderer.INSTANCE.renderStringAtPos(text, x, y, new Color(red, green, blue, alpha), shadow);
     }
-
-    public static class Vector {
-        public final float x;
-        public final float y;
-        public final float z;
-
-        public Vector(float x, float y, float z) {
-            this.x = x;
-            this.y = y;
-            this.z = z;
-        }
-
-        public float getX() {
-            return x;
-        }
-
-        public float getY() {
-            return y;
-        }
-
-        public float getZ() {
-            return z;
-        }
-
-        public float norm() {
-            return (float) Math.sqrt(x * x + y * y + z * z);
-        }
-
-        public Vector normalize() {
-            float n = norm();
-            return new Vector(x / n, y / n, z / n);
-        }
-    }
-
-    private static Vector Cross(Vector a, Vector b) {
-        float x = a.y * b.z - a.z * b.y;
-        float y = a.z * b.x - a.x * b.z;
-        float z = a.x * b.y - a.y * b.x;
-        return new Vector(x, y, z);
-    }
-
-    private static Vector Sub(Vector a, Vector b) {
-        return new Vector(a.x - b.x, a.y - b.y, a.z - b.z);
-    }
-
-    private static Vector Add(Vector a, Vector b) {
-        return new Vector(a.x + b.x, a.y + b.y, a.z + b.z);
-    }
-
-    private static Vector Mul(Vector a, float f) {
-        return new Vector(a.x * f, a.y * f, a.z * f);
-    }
-
-
 }
