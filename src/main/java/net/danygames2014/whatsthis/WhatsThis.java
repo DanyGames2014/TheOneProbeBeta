400c235b24e8a1a75a5ab62346d0a13f9fd7a5f0
Daniel Fildán <fildand@centrum.cz>
2025-08-10 21:39:59 +0200

    Temporarily disable accessory api compat

--- src/main/java/net/danygames2014/whatsthis/WhatsThis.java
+++ src/main/java/net/danygames2014/whatsthis/WhatsThis.java
@@ -71,7 +71,7 @@ public class WhatsThis {
         probe = new TemplateItem(NAMESPACE.id("probe")).setTranslationKey(NAMESPACE, "probe").setMaxCount(1);
         creativeProbe = new TemplateItem(NAMESPACE.id("creative_probe")).setTranslationKey(NAMESPACE, "creative_probe").setMaxCount(1);
 
-        if (accessoryApiCompat) {
+        if (accessoryApiCompat && false) {
             AccessoryApiCompat.registerProbeAccessory();
         }
     }
